5bc4ad62585989bfae8c9973ce83d9b52a9da547
George Olwal <olwalgeorge@live.com>
2025-11-09 04:28:17 +0300

    api-gateway: remove empty application.yml to silence Quarkus warning; exclude ORM/Agroal/JDBC deps to avoid datasource warning in gateway.

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -2,6 +2,15 @@ plugins {
     id("erp.quarkus-conventions")
 }
 
+configurations {
+    // API Gateway does not use a JDBC datasource; avoid Agroal/ORM warnings
+    implementation {
+        exclude(group = "io.quarkus", module = "quarkus-hibernate-orm")
+        exclude(group = "io.quarkus", module = "quarkus-agroal")
+        exclude(group = "io.quarkus", module = "quarkus-jdbc-postgresql")
+    }
+}
+
 dependencies {
     implementation(libs.quarkus.rest)
     implementation(libs.quarkus.rest.jackson)
